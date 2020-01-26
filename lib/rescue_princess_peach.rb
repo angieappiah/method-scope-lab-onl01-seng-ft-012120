7f43a67d986114377a56fa79413f9b0ff23149a0
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-26 06:06:17 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,9 +1,14 @@
-def rescue_princess_peach
-  status = "rescued"
-  puts "Hooray! Mario has rescued Princess Peach."
+def
+ "rescued"
+"Hooray! Mario has rescued Princess Peach."
 end
 
 ## Comment the below back in to see method scope in action!
 
 # rescue_princess_peach
 # puts status
+
+def rescue_princess_peach
+  phrase = 'rescued'
+  puts 'Hooray! Mario has "#{phrase}" Princess Peach.'
+end
